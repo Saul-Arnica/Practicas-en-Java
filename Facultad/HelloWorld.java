09b299d09ddec67119a644e94aa0356c2d764c96
Agustin <saulagustinarnica@outlook.com>
2024-08-09 21:07:56 -0300

    Realizando Ejercicios

--- Facultad/HelloWorld.java
+++ Facultad/HelloWorld.java
@@ -1,6 +1,5 @@
 package Facultad;
-// This is a simple Java program.
-// FileName : "HelloWorld.java".
+
 class HelloWorld {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
